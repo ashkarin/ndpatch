3ee2f10d373881842b3d90fd04a0b29f2c805e90
Andrei Shkarin <andrei.shkarin@gmail.com>
2019-05-08 19:32:04 +0200

    Increment the version to update the email in PyPI

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ndpatch',
-    version='0.0.1',
+    version='0.0.2',
     description='Extract arbitrary n-dimensional regions from ndarrays.',
     long_description=long_description,
     long_description_content_type='text/x-rst',
